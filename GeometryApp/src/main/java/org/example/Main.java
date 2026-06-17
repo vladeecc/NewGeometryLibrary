62150dbea233052ace74e66ca1d771aaea55a1fa
vladeecc <vladeecc123@icloud.com>
2026-06-17 09:49:14 +0300

    Initial commit

---
+++ GeometryApp/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+        Shape triangle = new Triangle(3, 4, 5);
+
+        System.out.println("Circle area: " + circle.getArea());
+        System.out.println("Circle perimeter: " + circle.getPerimeter());
+
+        System.out.println("Rectangle area: " + rectangle.getArea());
+        System.out.println("Rectangle perimeter: " + rectangle.getPerimeter());
+
+        System.out.println("Triangle area: " + triangle.getArea());
+        System.out.println("Triangle perimeter: " + triangle.getPerimeter());
+    }
+}
---
+++ GeometryLibrary/src/main/java/org/example/Triangle.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Triangle implements Shape {
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getArea() {
+        double p = getPerimeter() / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    public double getPerimeter() {
+        return a + b + c;
+    }
+}
