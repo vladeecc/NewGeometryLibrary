ae87945850d835c4b04f889c06cd15184fe6a2d1
vladeecc <vladeecc123@icloud.com>
2026-06-17 10:14:27 +0300

    Добавил GeometryUtils

--- GeometryApp/src/main/java/org/example/Main.java
+++ GeometryApp/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.utils.Converter;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
---
+++ GeometryUtils/src/main/java/org/utils/Converter.java
@@ -0,0 +1,12 @@
+package org.utils;
+
+public class Converter {
+
+    public static double cmToMeters(double cm) {
+        return cm / 100.0;
+    }
+
+    public static double metersToCm(double m) {
+        return m * 100.0;
+    }
+}
